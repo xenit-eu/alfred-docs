4d00e5071318553c6fba16a030b8ac538fc86cd6
kennethpi <kenneth.piot@xenit.eu>
2023-09-11 15:25:24 +0200

    XENFRED-4897 WIP

--- build-websites.sh
+++ build-websites.sh
@@ -91,26 +91,12 @@ rm -rf build/
 
 # Desktop
 build_docx_manual alfred-desktop 4.2 "Alfred Desktop User Guide 4.2.docx"
-split_manual alfred-desktop 4.2
+#split_manual alfred-desktop 4.2
 build_docx_manual alfred-desktop 4.1 "Alfred Desktop User Guide 4.1.docx"
-split_manual alfred-desktop 4.1
+#split_manual alfred-desktop 4.1
 build_docx_manual alfred-desktop 4.0 "Alfred Desktop User Guide 4.0.docx"
-split_manual alfred-desktop 4.0
-build_docx_manual alfred-desktop 3.8 "Alfred Desktop User Guide 3.8.docx"
-split_manual alfred-desktop 3.8
-build_docx_manual alfred-desktop 3.7 "Alfred Desktop User Guide 3.7.docx"
-split_manual alfred-desktop 3.7
-build_docx_manual alfred-desktop 3.6 "Alfred Desktop User Guide 3.6.docx"
-split_manual alfred-desktop 3.6
-build_docx_manual alfred-desktop 3.5 "Fred User Guide 3.5.docx"
-split_manual alfred-desktop 3.5
-build_docx_manual alfred-desktop 3.4 "Fred User Guide 3.4.docx"
-split_manual alfred-desktop 3.4
-build_docx_manual alfred-desktop 3.3 "Fred User Guide 3.3.docx"
-split_manual alfred-desktop 3.3
-build_docx_manual alfred-desktop 3.2 "Fred User Guide Trial 3.2.docx"
-split_manual alfred-desktop 3.2
-build_product_website alfred-desktop
+#split_manual alfred-desktop 4.0
+#build_product_website alfred-desktop
 
 # Finder
 build_and_split_manual alfred-finder 2.5-user "user-guide.md"
