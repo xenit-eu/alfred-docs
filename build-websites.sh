37095e694d6df4a40da8fda012bbbed668a54bf9
kennethpi <kenneth.piot@xenit.eu>
2023-09-12 08:50:44 +0200

    XENFRED-4897 WIP

--- build-websites.sh
+++ build-websites.sh
@@ -97,7 +97,7 @@ build_docx_manual alfred-desktop 3.8 "Alfred Desktop User Guide 3.8.docx"
 split_manual alfred-desktop 4.2
 split_manual alfred-desktop 4.1
 split_manual alfred-desktop 4.0
-split_manual alfred-desktop 3.8
+#split_manual alfred-desktop 3.8
 #build_docx_manual alfred-desktop 3.7 "Alfred Desktop User Guide 3.7.docx"
 #split_manual alfred-desktop 3.7
 #build_docx_manual alfred-desktop 3.6 "Alfred Desktop User Guide 3.6.docx"
